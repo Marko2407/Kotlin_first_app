30b31b4ed81cbe55b51d6294cff9c0cbc8011a80
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-19 14:22:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kotlinfun/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlinfun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textView:TextView
+    var count = 0
+
+    fun reset(view: View) {
+        count =0
+        textView.setText(count.toString())
+    }
+
+    fun plusOne(view: View) {
+        count ++
+        textView.setText(count.toString())
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      textView  = findViewById<TextView>(R.id.textView)
+        textView.setText("hello fam")
+
+        }
+
+    }
+
+
+
